3307908d4e7a6f03577f6d6f849db4ad1e1fbbbe
Vilhelm Melkstam <vilhelm.melkstam@itggot.se>
2017-03-20 22:27:55 +0100

    Added new memes

--- bot.rb
+++ bot.rb
@@ -2,8 +2,8 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: 'Mjg2ODg2MDY1NDg5MzEzNzky.C5nRvw.xE95fyrzIO3Mi1lprOUuSLEn7qA', client_id: 286886065489313792
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.message(containing: 'kapitalism') do |event|
+  event.respond 'Hoppas du inte säger något bra om kapitalismen, länge leve kommunismen!'
 end
 
 bot.message(with_text: 'meme') do |event|
